3af1698d261d6e03525c5ab5149bf7529bd1f028
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-16 16:02:05 -0600

    :crystal_ball: Built out functionality for Hotel Methods; findTodaysBookings; findTotalAvailableRooms; findTotalRevenueForToday; findPercentageOfOccupiedRooms

--- src/Hotel.js
+++ src/Hotel.js
@@ -2,9 +2,56 @@ class Hotel {
   constructor(rooms, bookings, date) {
     this.rooms = rooms;
     this.bookings = bookings;
+    this.todaysBookings = this.findTodaysBookings();
     this.date = date;
   }
-  
+
+  findTodaysBookings() {
+    let todaysBookings = this.bookings.filter(booking => booking.date === this.date)
+    // domUpdates.displayTodaysBookings(todaysBookings)
+    return todaysBookings
+  }
+
+  findTotalAvailableRooms() {
+    let availableRoomNumbers = this.bookings.reduce((unbookedRooms, booking) => {
+      if(booking.date !== this.date) {
+        unbookedRooms.push(booking.roomNumber)
+      }
+      return unbookedRooms
+    }, [])
+
+    let allAvailableRooms = []
+    availableRoomNumbers.forEach(roomNumber => {
+      this.rooms.forEach(room => {
+        if(roomNumber === room.number) {
+          allAvailableRooms.push(room)
+        }
+      })
+    })
+    // domUpdates.displayAvailableRooms(allAvailableRooms)
+    return allAvailableRooms
+  }
+
+  findTotalRevenueForToday() {
+    let todaysBookings = this.bookings.filter(booking => booking.date === this.date)
+    let todaysRevenue = todaysBookings.reduce((revenue, booking) => {
+      this.rooms.forEach(room => {
+        if(room.number === booking.roomNumber) {
+          revenue += room.costPerNight
+        }
+      })
+      return revenue
+    }, 0)
+    // domUpdates.displayTodaysRevenue(todaysRevenue)
+    return todaysRevenue
+  }
+
+  findPercentageOfOccupiedRooms(date) {
+    let numberOfRoomsBookedToday = this.bookings.filter(booking => booking.date === this.date).length
+    let percentageOfOccupiedRooms = +((numberOfRoomsBookedToday/this.rooms.length).toFixed(2) * 100)
+    // domUpdates.displayPercentageOfOccupiedRooms(percentageOfOccupiedRooms)
+    return percentageOfOccupiedRooms
+  }
 
 }
 
---
+++ src/domUpdates.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import Hotel from '../src/Hotel';
+import Guest from '../src/Guest';
+import Manager from '../src/Guest';
+
+let domUpdates = {
+
+  displayManagerDashboard() {
+
+  },
+
+  displayTodaysBookings(todaysBookings) {
+
+  },
+
+  displayAvailableRooms(allAvailableRooms) {
+
+  },
+
+  displayTodaysRevenue(todaysRevenue) {
+
+  },
+
+  displayPercentageOfOccupiedRooms(percentageOfOccupiedRooms) {
+
+  },
+
+
+}
+
+export default domUpdates;
