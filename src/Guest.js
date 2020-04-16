48062f7db323687c6787c58e0b5930ae71ed281a
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-16 16:04:02 -0600

    :crystal_ball: Modified constructor slightly; Added method names

--- src/Guest.js
+++ src/Guest.js
@@ -3,14 +3,21 @@ class Guest {
     this.id = id;
     this.name = name;
     this.firstName = this.findFirstName();
-    
-
   }
 
   findFirstName() {
     let firstName = this.name.split(' ');
     return firstName[0]
   }
+
+  findAllBookings() {
+
+  }
+
+  createBooking() {
+
+  }
+
 };
 
 export default Guest;
