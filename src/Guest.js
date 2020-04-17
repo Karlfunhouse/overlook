f79fbf34a4e8fc97d2373ac80aa0f7738d12d01d
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-16 20:10:57 -0600

    :crystal_ball: Added functionality for findFirstName; findMyBookings; calculateTotalSpent methods

--- src/Guest.js
+++ src/Guest.js
@@ -1,20 +1,39 @@
+import domUpdates from './domUpdates'
+
 class Guest {
-  constructor(id, name, hotelData) {
+  constructor(id, name, rooms, bookings) {
     this.id = id;
     this.name = name;
+    this.rooms = rooms;
+    this.bookings = bookings;
     this.firstName = this.findFirstName();
+    this.myBookings = this.findMyBookings;
   }
 
   findFirstName() {
     let firstName = this.name.split(' ');
+    // domUpdates.displayFirstName(firstName);
     return firstName[0]
   }
 
-  findAllBookings() {
+  findMyBookings() {
+    let myBookings = this.bookings.filter(booking => booking.userID === this.id)
+    //domUpdates.displayMyBookings(myBookings);
+    return myBookings
 
   }
 
-  createBooking() {
+  calculateTotalSpent() {
+    let totalMoneySpent = this.findMyBookings().reduce((totalSpent, booking) => {
+      let foundRoom = this.rooms.find(room => room.number === booking.roomNumber)
+      totalSpent += foundRoom.costPerNight
+      return totalSpent
+    }, 0).toFixed(2)
+    // domUpdates.displayTotalSpent(+totalMoneySpent)
+    return +totalMoneySpent
+  }
+
+  bookARoom() {
 
   }
 
