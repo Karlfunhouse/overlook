ccb83209a3b288c6b662e40bb18ef2520ad0330d
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-17 21:26:18 -0600

    :crystal_ball: Functionality to load guest name & total spent in DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import $ from 'jquery';
 import Hotel from './Hotel';
@@ -11,7 +5,6 @@ import Guest from './Guest';
 import Manager from './Manager';
 import domUpdates from './domUpdates'
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/moon-icon.svg'
 
 //Global Variables
@@ -25,6 +18,21 @@ let hotel;
 let date;
 let manager;
 let guest;
+
+//LOGIN
+function checkLogin() {
+  event.preventDefault();
+  let userName = $('.username-js').val().toLowerCase();
+  if (userName.toLowerCase() === 'manager' && $('.password-js').val() === 'overlook2020') {
+    managerFetch();
+  } else if ((userName.includes('customer') && $('.password-js').val() === 'overlook2020')) {
+    let splitUserName = userName.split('customer');
+    let guestId = splitUserName[1];
+    guestFetch(guestId)
+  } else {
+    domUpdates.displayLoginError()
+  }
+}
 
 //FETCH
 function managerFetch() {
@@ -52,27 +60,9 @@ function managerFetch() {
     .then(() => {
       hotel = new Hotel(rooms, bookings, "2020/02/04");
       console.log('hotel', hotel);
-
       let manager = new Manager(0, 'Boss', rooms, bookings)
-        // loadManagerPage(date)
-        console.log(manager);
-      // return
-      // } else {
-      //   instantiateGuest(guests, rooms, bookings, guestId)
-        // let guest = guests.users.find(guest => guest.id === +guestId)
-        // let guestBookings = bookings.bookings.filter(booking => booking.userID === +guestId)
-        // let guestRooms = []
-        // let bookingInfo = guestBookings.forEach(booking => {
-        //   rooms.rooms.forEach(room => {
-        //     if (room.number === booking.roomNumber) {
-        //       // booking.roomInfo = {room}
-        //       guestRooms.push(room)
-        //     }
-        //   })
-        // })
-        // let currentGuest = new Guest(guest.id, guest.name, guestRooms, guestBookings)
-        // console.log('currentGuest', currentGuest);
-
+      console.log(manager);
+      //Load Manager Page
   })
     .catch(error => {
       console.log('Something is amiss with promise all', error)
@@ -104,20 +94,11 @@ function guestFetch(guestId) {
     .then(() => {
       hotel = new Hotel(rooms, bookings, "2020/02/04");
       console.log('hotel', hotel);
-      // instantiateGuest(guests, rooms, bookings, guestId)
-      let guest = guests.users.find(guest => guest.id === +guestId)
-      let guestBookings = bookings.bookings.filter(booking => booking.userID === +guestId)
-      let guestRooms = []
-      let bookingInfo = guestBookings.forEach(booking => {
-        rooms.rooms.forEach(room => {
-          if (room.number === booking.roomNumber) {
-            // booking.roomInfo = {room}
-            guestRooms.push(room)
-          }
-        })
-      })
-      let currentGuest = new Guest(guest.id, guest.name, guestRooms, guestBookings)
-      console.log('currentGuest', currentGuest);
+    })
+    .then(() => {
+      let guest = instantiateGuest(guests, rooms, bookings, guestId)
+      console.log('100', guest)
+      displayGuestPage(guest)
     })
     .catch(error => {
       console.log('Something is amiss with promise all', error)
@@ -127,27 +108,7 @@ function guestFetch(guestId) {
 //EVENTS
 $('.login-submit-js').on('click', (event) => checkLogin());
 
-//LOGIN
-function checkLogin() {
-  event.preventDefault();
-  let userName = $('.username-js').val().toLowerCase();
-  if (userName.toLowerCase() === 'manager' && $('.password-js').val() === 'overlook2020') {
-    // let manager
-    managerFetch();
-    //load manager page
-    // instantiateUser('manager')
-  } else if ((userName.includes('customer') && $('.password-js').val() === 'overlook2020')) {
-    // let guest;
-    let splitUserName = userName.split('customer');
-    let guestId = splitUserName[1];
-    guestFetch(guestId)
-    // loadGuestPage(guestId, guests)
-    // instantiateUser('customer')
-    //do guest stuff
-  } else {
-    //display login error
-  }
-}
+
 
 function instantiateGuest(guests, rooms, bookings, guestId) {
   let guest = guests.users.find(guest => guest.id === +guestId)
@@ -156,25 +117,26 @@ function instantiateGuest(guests, rooms, bookings, guestId) {
   let bookingInfo = guestBookings.forEach(booking => {
     rooms.rooms.forEach(room => {
       if (room.number === booking.roomNumber) {
-        // booking.roomInfo = {room}
         guestRooms.push(room)
       }
     })
   })
   let currentGuest = new Guest(guest.id, guest.name, guestRooms, guestBookings)
-  console.log('currentGuest', currentGuest);
+  // console.log('currentGuest', currentGuest);
+  // domUpdates.loadGuestPage(currentGuest)
   return currentGuest
+}
 
+function displayGuestPage(guest) {
+  guest.findFirstName()
+  console.log('firstName', guest.findFirstName())
+  guest.findMyBookings();
+  console.log('bookings', guest.findMyBookings());
+  guest.calculateTotalSpent();
+  console.log('total spent', guest.calculateTotalSpent());
 }
 
 
-function loadGuestPage(guestId, guests) {
-  // console.log(guestId);
-  // let currentGuest = guests.users.find(guest => guest.id === guestId)
-  // console.log(currentGuest);
-
-  // guest = new Guest (currentGuest.id, currentGuest.name, rooms, bookings)
-}
 
 function loadManagerPage() {
 
