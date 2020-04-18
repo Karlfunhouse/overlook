3acefac35fe55548bcd7694557f3a4147dda67e9
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-18 15:38:30 -0600

    :crystal_ball: Added Sort feature to display bookings

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,19 +1,32 @@
+import domUpdates from './domUpdates'
+var Moment = require('moment');
+var todaysDate = Number(Moment().format('YYYY/MM/DD').split('/').join(''))
+
 class Hotel {
   constructor(rooms, bookings, date) {
     this.rooms = rooms;
     this.bookings = bookings;
-    this.todaysBookings = this.findTodaysBookings();
     this.date = date;
+    this.todaysBookings = this.findTodaysBookings;
+  }
+
+  setUpHotel() {
+    this.sortBookingsByDate();
+    this.findTodaysBookings();
+    this.findTotalAvailableRooms();
+    this.findTotalRevenueForToday();
+    this.findPercentageOfOccupiedRooms();
+
   }
 
   findTodaysBookings() {
-    let todaysBookings = this.bookings.filter(booking => booking.date === this.date)
+    let todaysBookings = this.bookings.bookings.filter(booking => booking.date === this.date)
     // domUpdates.displayTodaysBookings(todaysBookings)
     return todaysBookings
   }
 
   findTotalAvailableRooms() {
-    let availableRoomNumbers = this.bookings.reduce((unbookedRooms, booking) => {
+    let availableRoomNumbers = this.bookings.bookings.reduce((unbookedRooms, booking) => {
       if(booking.date !== this.date) {
         unbookedRooms.push(booking.roomNumber)
       }
@@ -22,7 +35,7 @@ class Hotel {
 
     let allAvailableRooms = []
     availableRoomNumbers.forEach(roomNumber => {
-      this.rooms.forEach(room => {
+      this.rooms.rooms.forEach(room => {
         if(roomNumber === room.number) {
           allAvailableRooms.push(room)
         }
@@ -33,9 +46,9 @@ class Hotel {
   }
 
   findTotalRevenueForToday() {
-    let todaysBookings = this.bookings.filter(booking => booking.date === this.date)
+    let todaysBookings = this.bookings.bookings.filter(booking => booking.date === this.date)
     let todaysRevenue = todaysBookings.reduce((revenue, booking) => {
-      this.rooms.forEach(room => {
+      this.rooms.rooms.forEach(room => {
         if(room.number === booking.roomNumber) {
           revenue += room.costPerNight
         }
@@ -47,10 +60,23 @@ class Hotel {
   }
 
   findPercentageOfOccupiedRooms() {
-    let numberOfRoomsBookedToday = this.bookings.filter(booking => booking.date === this.date).length
+    let numberOfRoomsBookedToday = this.bookings.bookings.filter(booking => booking.date === this.date).length
     let percentageOfOccupiedRooms = +((numberOfRoomsBookedToday/this.rooms.length).toFixed(2) * 100)
     // domUpdates.displayPercentageOfOccupiedRooms(percentageOfOccupiedRooms)
     return percentageOfOccupiedRooms
+  }
+
+  filterRoomsByType(roomType) {
+    let filteredRooms = this.rooms.rooms.filter(room => room.roomType === roomType)
+    // domUpdates.displayFilteredRoomsByType(filteredRooms)
+    return filteredRooms
+  }
+
+  sortBookingsByDate() {
+    // console.log('hotel bookings', this.bookings.bookings);
+    let sortedBookings = this.bookings.bookings.sort((a,b) => new Moment(b.date).format('YYYYMMDD') - new Moment(a.date).format('YYYYMMDD'))
+    sortedBookings = this.bookings
+    // console.log('sortedBookings', sortedBookings);
   }
 
 }
