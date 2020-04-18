9584a72a209d88e57e92a0cd3db92ebecaf848c3
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-18 11:39:32 -0600

    :crystal_ball: Refactored Guest class to have all roomInfo within bookings

--- src/Guest.js
+++ src/Guest.js
@@ -1,10 +1,10 @@
 import domUpdates from './domUpdates'
 
 class Guest {
-  constructor(id, name, rooms, bookings) {
+  constructor(id, name, bookings) {
     this.id = id;
     this.name = name;
-    this.rooms = rooms;
+    // this.rooms = rooms;
     this.bookings = bookings;
     this.firstName = this.findFirstName();
     this.myBookings = this.findMyBookings;
@@ -12,24 +12,23 @@ class Guest {
 
   findFirstName() {
     let firstName = this.name.split(' ');
-    // domUpdates.displayFirstName(firstName);
+    domUpdates.displayFirstName(firstName[0]);
     return firstName[0]
   }
 
   findMyBookings() {
     let myBookings = this.bookings.filter(booking => booking.userID === this.id)
-    //domUpdates.displayMyBookings(myBookings);
+    domUpdates.displayMyBookings(myBookings);
+    // console.log(myBookings);
     return myBookings
-
   }
 
   calculateTotalSpent() {
     let totalMoneySpent = this.findMyBookings().reduce((totalSpent, booking) => {
-      let foundRoom = this.rooms.find(room => room.number === booking.roomNumber)
-      totalSpent += foundRoom.costPerNight
+      totalSpent += booking.roomInfo.costPerNight
       return totalSpent
-    }, 0).toFixed(2)
-    // domUpdates.displayTotalSpent(+totalMoneySpent)
+    }, 0)
+    domUpdates.displayTotalSpent(+totalMoneySpent)
     return +totalMoneySpent
   }
 
