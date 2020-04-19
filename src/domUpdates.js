072cd73f9102cd2a762950768178b84bfeafe715
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-19 14:19:24 -0600

    :crystal_ball: added functionality to display open room #s

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -28,9 +28,14 @@ let domUpdates = {
     todaySortedBookings.forEach(booking => {
       $('.bookings-today').append(`
         <article id=${booking.id} class='booking'>
-        <h2>GuestID: ${booking.userID}</h2>
+        <h2>Room # ${booking.roomNumber}</h2>
+        <h3>GuestID: ${booking.userID}</h3>
         <h4>Date: ${booking.date}<h4>
-        <h3>Room # ${booking.roomNumber}</h3>
+        <h4>Room Type: ${booking.roomType}</h4>
+        <h4>Bed Size: ${booking.bedSize}</h4>
+        <h4># of Beds: ${booking.numBeds}</h4>
+        <h4>Cost/Night $${booking.costPerNight}</h4>
+        <h4>Bidet: ${booking.bidet}</h4>
       </article>`)
     })
   },
@@ -42,6 +47,10 @@ let domUpdates = {
   // <h3>Bidet: ${booking.roomInfo.bidet}</h3>
   displayNumberOfAvailableRooms(allAvailableRooms) {
     $('.available-rooms').text(`Available Rooms: ${allAvailableRooms}`)
+  },
+
+  displayOpenRoomsForToday(todaysOpenRooms) {
+    $('.available-room-numbers').text(`Open Room #'s: ${todaysOpenRooms}`)
   },
 
   displayTodaysRevenue(todaysRevenue) {
@@ -66,11 +75,11 @@ let domUpdates = {
       `<article id=${booking.id} class='booking'>
         <h4>Date: ${booking.date}<h4>
         <h3>Room #: ${booking.roomNumber}</h3>
-        <h3>Room Type: ${booking.roomInfo.roomType}</h3>
-        <h3>Bed Size: ${booking.roomInfo.bedSize}</h3>
-        <h3># of Beds: ${booking.roomInfo.numBeds}</h3>
-        <h3>$/Night: ${booking.roomInfo.costPerNight}</h3>
-        <h3>Bidet: ${booking.roomInfo.bidet}</h3>
+        <h3>Room Type: ${booking.roomType}</h3>
+        <h3>Bed Size: ${booking.bedSize}</h3>
+        <h3># of Beds: ${booking.numBeds}</h3>
+        <h3>$/Night: ${booking.costPerNight}</h3>
+        <h3>Bidet: ${booking.bidet}</h3>
       </article>`)
     })
   },
