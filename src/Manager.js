eb09fe2012f03d3fbd0c9e17a16780ac16af9128
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-04-19 19:07:37 -0600

    :crystal_ball: Adding Moment.js

--- src/Manager.js
+++ src/Manager.js
@@ -1,5 +1,6 @@
 import Guest from '../src/Guest';
 import domUpdates from './domUpdates'
+const moment = require('moment')
 
 
 class Manager extends Guest {
@@ -9,6 +10,7 @@ class Manager extends Guest {
     this.name = name;
     this.bookings = bookings;
     this.rooms = rooms;
+    this.today = moment().format('YYYY/MM/DD');
   }
 
   deleteBooking() {
